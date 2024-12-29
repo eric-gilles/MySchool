e11451dcca68fc7303d7fc70c0cbb6eb9f48f691
Éric GILLES <gilleseric11@gmail.com>
2024-12-29 21:47:28 +0100

    feat: add SonarQube to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,13 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("org.sonarqube") version "6.0.1.5171"
+}
+
+sonar {
+  properties {
+      property("sonar.projectKey", "eric-gilles_MySchool")
+      property("sonar.organization", "eric-gilles-1")
+      property("sonar.host.url", "https://sonarcloud.io")
+  }
 }
